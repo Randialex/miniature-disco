b48ec0fb5399dcee0dc4819e8c2c2ace8217147b
Randialex <cj7hj6m8j8@privaterelay.appleid.com>
2026-08-12 19:04:55 +0800

    Delete app directory

--- app/(main)/owl-post/page.tsx
+++
@@ -1,7 +0,0 @@
-"use client";
-
-import MailboxAuthGate from "@/components/MailboxAuthGate";
-import MailboxDashboard from "@/components/MailboxDashboard";
-import { useMailbox } from "@/components/MailboxProvider";
-
-export default function OwlPostPage(){const {member}=useMailbox();return member?<MailboxDashboard/>:<MailboxAuthGate/>;}
